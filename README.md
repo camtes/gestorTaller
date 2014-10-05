e030667058ba12da8f862610ce145fa730808b77
Carlos Campos <carloscamposfuentes@gmail.com>
2014-10-05 22:24:09 +0200

    update info

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 gestorTaller
 ============
 
-Gestión de la entrada y salida de un taller informático
+Gestión de la entrada y salida de un taller informático.
+
+Para la instalación, es necesario tener una base de datos MySQL y seguir los siguientes pasos:
+1. Crear la base de datos a través del archivo llamado **"base-de-datos.txt"**
+2. Configurar el archivo configuracion/configuracion.php
+	1. Modificar la variable llamada **"host"**
+
+Si tienes algún problema en la instalación no dudes en contactar conmigo carlos(arroba)ccamposfuentes.es
--- configuracion/configuracion.php
+++ configuracion/configuracion.php
@@ -1,9 +1,11 @@
 <?php
+	// IP del host donde se aloja la base de datos
+	$host = "192.168.56.101" ;
 
-// Conexión a base de datos
-define("DB_DSN", "mysql:host=192.168.56.101;dbname=gestorTaller");
-define("DB_USUARIO", "gTaller_user");
-define("DB_CONTRASENA", "g3stor");
-define("TABLA_CLIENTE", 'cliente');
-define("TABLA_SAT", 'sat');
+	// Conexión a base de datos (NO MODIFICAR)
+	define("DB_DSN", "mysql:host=$host;dbname=gestorTaller");
+	define("DB_USUARIO", "gTaller_user");
+	define("DB_CONTRASENA", "g3stor");
+	define("TABLA_CLIENTE", 'cliente');
+	define("TABLA_SAT", 'sat');
 ?>
