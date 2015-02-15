b68dd322f3f255abf109ab6a537c28b57f70a3ad
ccamposfuentes <carlos@ccamposfuentes.es>
2015-02-15 13:53:53 +0100

    Actualizo script BBDD y codificacion

--- script/gestorTaller.sql
+++ script/gestorTaller.sql
@@ -10,7 +10,7 @@ CREATE TABLE `cliente` (
   `telefono2` int(11),
   `direccion` varchar(50),
   PRIMARY KEY (`id_cliente`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf-8;
 
 --
 -- Table structure for table `sat`
@@ -22,6 +22,8 @@ CREATE TABLE `sat` (
   `rep` varchar(9) NOT NULL,
   `problema` varchar(255) NOT NULL,
   `informe` varchar(255) DEFAULT NULL,
+  `fecha_entrada`date NOT NULL,
+  `fecha_salida`date NULL,
   `piezas` varchar(255) DEFAULT NULL,
   `precio_rep` double DEFAULT NULL,
   `precio_piezas` double DEFAULT NULL,
@@ -29,14 +31,14 @@ CREATE TABLE `sat` (
   `estado` tinyint(1) NOT NULL,
   PRIMARY KEY (`id_sat`),
   KEY `id_cliente` (`id_cliente`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf-8;
 
 
 UNLOCK TABLES;
 
 --
 -- Si no tienes ningún usuario para la base de datos de gestorTaller, con las dos líneas de abajo
--- 
+--
 --
 -- grant all privileges on gestorTaller.* to 'gTaller_user'@'%' identified by 'g3stor' with grant option;
 -- flush privileges;
